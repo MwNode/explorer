ad597fbfb80b2fbbebfa20c1c204b835e15e553a
Matt Ruan <matt@namtech.com.au>
2024-06-19 00:50:53 +0700

    fix eslint

--- src/components/dynamic/TextElement.vue
+++ src/components/dynamic/TextElement.vue
@@ -43,7 +43,7 @@ const text = computed(() => {
 const names = ref([] as {name?: string | null, provider?: string}[])
 
 onMounted(() => {
-  if(isAddress()) nameMatcha.lookupAll(props.value).then(re => {
+  if(isAddress()) nameMatcha.lookupAll(props.value).then((re: any) => {
     names.value = Object.keys(re).map(key => ({name: re[key], provider: key})).filter( x => x.name)
   })
 })
