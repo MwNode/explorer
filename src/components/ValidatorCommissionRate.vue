fb2b608b1429e02f05aa647d96320251d4515124
Matt Ruan <matt@namtech.com.au>
2024-06-28 16:10:40 +0700

    update spacing box Commission Rate - page StackingID

--- src/components/ValidatorCommissionRate.vue
+++ src/components/ValidatorCommissionRate.vue
@@ -119,7 +119,7 @@ const chartConfig = computed(() => {
 </script>
 
 <template>
-    <div class="bg-white-10 rounded shadow px-4 pb-4 pt-8">
+    <div class="bg-white-10 rounded shadow px-4 pb-8 pt-4">
         <div class="text-lg text-main font-semibold mb-1">Commission Rate</div>
         <div class="text-sm text-gray-3">
             {{ `Updated at ${format.toDay(props.commission?.update_time, 'short')}` }}
